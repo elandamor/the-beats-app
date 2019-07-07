3a321e080716d1a89af3703590d34235bcf399ed
Thandolwethu <mpofuthandolwethu@gmail.com>
2019-07-07 13:26:07 +0200

    refactor(Artist): Setup and improve schema. Introduce createArtist mutation. Update resolvers and services.

---
+++ packages/backend/src/schema/Artist/Artist.service.ts
@@ -0,0 +1,58 @@
+import { Context } from "../../utils";
+import { ArtistCreateInput } from "../../generated/prisma-client";
+import { UnknownError } from "../../utils/errors";
+
+/**
+ * Creates an artist
+ * @param artist - Object of artist to create
+ * @param context - Exposes prisma
+ */
+export const createArtist = async (artist, { prisma }: Context) => {
+  const { alias, name } = artist;
+
+  const artistExists = await prisma.$exists.artist({ alias: artist.alias });
+
+  if (artistExists) {
+    return await prisma.artist({ alias: artist.alias });
+  }
+
+  const payload: ArtistCreateInput = {
+    alias,
+    name
+  };
+
+  try {
+    const createdArtist = await prisma.createArtist({ ...payload });
+
+    return createdArtist;
+  } catch (error) {
+    throw new UnknownError({
+      message: error.message
+    });
+  }
+};
+
+/**
+ * Creates many artists
+ * @param artists - Array of artists to create
+ * @param context - Exposes prisma
+ */
+export const createArtists = async (artists, context: Context) => {
+  if (!artists) {
+    return;
+  }
+
+  return new Promise<Array<{ alias: string }>>(resolve => {
+    const createdArtists = [];
+
+    artists.forEach((artist, index) => {
+      createArtist(artist, context).then(createdArtist => {
+        createdArtists.push({ id: createdArtist.id });
+
+        if (index === artists.length - 1) {
+          resolve(createdArtists);
+        }
+      });
+    });
+  });
+};
