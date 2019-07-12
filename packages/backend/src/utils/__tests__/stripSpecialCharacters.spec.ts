65b308c502c48c2efb3906f67da2152a5bf2bbda
Thandolwethu <mpofuthandolwethu@gmail.com>
2019-07-12 22:28:18 +0200

    tests(stripSelectCharacters): Add second test. To comply with minimum of 2 tests per unit.

--- packages/backend/src/utils/__tests__/stripSpecialCharacters.spec.ts
+++ packages/backend/src/utils/__tests__/stripSpecialCharacters.spec.ts
@@ -1,8 +1,15 @@
 import { stripSpecialCharacters } from "../stripSpecialCharacters";
 
 describe("stripSpecialCharacters", () => {
-  it("removes all special characters from a string", () => {
+  it("should remove all special characters from a simple string", () => {
     const input = stripSpecialCharacters("A1 Beats - Jan'19");
+    const expectedResult = "A1BeatsJan19";
+
+    expect(input).toBe(expectedResult);
+  });
+
+  it("should remove all special characters from a complex string", () => {
+    const input = stripSpecialCharacters("#A1_Beats - Jan(19)");
     const expectedResult = "A1BeatsJan19";
 
     expect(input).toBe(expectedResult);
