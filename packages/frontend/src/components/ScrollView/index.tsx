3f172322ed0fe6fdaf2388214e0c157e303692d5
Thandolwethu <mpofuthandolwethu@gmail.com>
2019-08-03 06:58:32 +0200

    refactor(ScrollView): Make to detect content overflow. Should update underlying styling to correctly justify overflowing content

--- packages/frontend/src/components/ScrollView/index.tsx
+++ packages/frontend/src/components/ScrollView/index.tsx
@@ -1,5 +1,7 @@
+import React, { FC, useState } from 'react';
 import styled from 'styled-components';
 import { StyledSystemProps } from 'styled-system';
+import Measure from 'react-measure';
 
 import Box from '../Box';
 
@@ -16,10 +18,45 @@ export interface IScrollViewProps extends StyledSystemProps {}
  * <ScrollView />
  */
 
-const ScrollView = styled(Box)<IScrollViewProps>`
+const Wrapper = styled(Box)`
   -webkit-overflow-scrolling: touch;
   overscroll-behavior: contain;
 `;
+
+const ScrollView: FC<IScrollViewProps> = ({ children, ...rest }) => {
+  const [contentHeight, setContentHeight] = useState<number>(-1);
+  const [wrapperHeight, setWrapperHeight] = useState<number>(-1);
+
+  const isOverflowing = contentHeight > wrapperHeight;
+
+  return (
+    <Measure
+      bounds
+      onResize={(rect) => {
+        setWrapperHeight(rect.bounds!.height);
+      }}
+    >
+      {({ measureRef: wrapperRef }) => (
+        <Wrapper
+          {...rest}
+          ref={wrapperRef}
+          justifyContent={isOverflowing ? 'flex-start' : rest.justifyContent}
+        >
+          <Measure
+            bounds
+            onResize={(rect) => {
+              setContentHeight(rect.bounds!.height);
+            }}
+          >
+            {({ measureRef: contentRef }) => (
+              <div ref={contentRef}>{children}</div>
+            )}
+          </Measure>
+        </Wrapper>
+      )}
+    </Measure>
+  );
+};
 
 ScrollView.defaultProps = {
   height: '100%',
