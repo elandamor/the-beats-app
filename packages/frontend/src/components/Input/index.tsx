726d598e51014984755f06878f1546564420d226
Thandolwethu <mpofuthandolwethu@gmail.com>
2019-08-06 08:46:25 +0200

    refactor(Input): Allow for `file` type

--- packages/frontend/src/components/Input/index.tsx
+++ packages/frontend/src/components/Input/index.tsx
@@ -7,13 +7,15 @@ import { Text } from '@app/typography';
 import Flex from '../Flex';
 import Button from '../Button';
 import { FiEye, FiEyeOff } from 'react-icons/fi';
+import Dropzone from '../Dropzone/Loadable';
 
 export interface IInputProps extends FieldProps {
   autoComplete?: string;
+  hideLabel?: boolean;
   label: string;
   placeholder?: string;
   readOnly?: boolean;
-  type?: 'text' | 'email' | 'number' | 'password';
+  type?: 'text' | 'email' | 'number' | 'password' | 'file';
 }
 
 /**
@@ -29,26 +31,39 @@ export interface IInputProps extends FieldProps {
  * />
  */
 
-const Input: FC<IInputProps> = ({ field, label, ...rest }) => {
+const Input: FC<IInputProps> = ({ field, hideLabel, label, ...rest }) => {
   const [passwordVisible, setPasswordVisible] = useState(false);
   const isPasswordInput = rest.type === 'password';
+
+  const inputProps = {
+    ...field,
+    'aria-label': label,
+    id: field.name,
+    placeholder: rest.placeholder,
+    readOnly: rest.readOnly,
+    type: passwordVisible ? 'text' : rest.type,
+    ...(rest.autoComplete ? { autoComplete: rest.autoComplete } : {}),
+  };
+
+  const renderInput = () => {
+    switch (rest.type) {
+      case 'file':
+        return <Dropzone {...inputProps} />;
+      default:
+        return <DefaultInput {...inputProps} />;
+    }
+  };
 
   return (
     <Box mb="2">
       <label htmlFor={field.name}>
-        <Flex mb="1">
-          <Text>{label}</Text>
-        </Flex>
+        {!hideLabel && (
+          <Flex mb="1">
+            <Text mb="0">{label}</Text>
+          </Flex>
+        )}
         <Box alignItems="center" flexDirection="row">
-          <DefaultInput
-            {...field}
-            aria-label={label}
-            id={field.name}
-            placeholder={rest.placeholder}
-            readOnly={rest.readOnly}
-            type={passwordVisible ? 'text' : rest.type}
-            {...(rest.autoComplete ? { autoComplete: rest.autoComplete } : {})}
-          />
+          {renderInput()}
           {isPasswordInput && (
             <Button
               variant="icon"
