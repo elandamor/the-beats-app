7611066268ced62a9f83ae3b937b245cb881166c
Thandolwethu <mpofuthandolwethu@gmail.com>
2019-08-17 14:21:03 +0200

    refactor(containers): Update index

--- packages/frontend/src/containers/index.ts
+++ packages/frontend/src/containers/index.ts
@@ -1,6 +1,10 @@
 // Getters
 export { default as GetAlbums } from './GetAlbums/Loadable';
 export { default as GetArtists } from './GetArtists/Loadable';
+export { default as GetPlaylists } from './GetPlaylists/Loadable';
 export { default as GetTracks } from './GetTracks/Loadable';
 // Setters
 export { default as AddAlbum } from './AddAlbum/Loadable';
+// Other
+export { default as Login } from './Login/Loadable';
+export { default as Register } from './Register/Loadable';
