98d351a913927b408fd36c48bae268cb508805e9
Thandolwethu <mpofuthandolwethu@gmail.com>
2019-08-17 14:20:05 +0200

    chore: Remove unused components.

--- packages/frontend/src/components/index.ts
+++ packages/frontend/src/components/index.ts
@@ -13,8 +13,6 @@ export { default as Modal } from './Modal/Loadable';
 export { default as NavigationBar } from './NavigationBar/Loadable';
 export { default as Routes } from './Routes/Loadable';
 export { default as ScrollView } from './ScrollView';
-export { default as WrappedMutation } from './WrappedMutation';
-export { default as WrappedQuery } from './WrappedQuery';
 // Project Specific
 export { default as Album } from './Album';
 export { default as Artist } from './Artist';
