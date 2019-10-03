2379048f08d03d89f364d8439b6c489fa030274d
mpofuthandolwethu <mpofuthandolwethu@gmail.com>
2019-10-04 00:38:40 +0200

    refactor(seed.ts): Update seed data

---
+++ packages/backend/prisma/data/albums.ts
@@ -0,0 +1,306 @@
+export default [
+  {
+    artists: [
+      {
+        name: "Kygo"
+      }
+    ],
+    genres: ["House"],
+    name: "Happy Now - Single",
+    releaseDate: "2018-10-26T00:00:00.000Z",
+    releaseType: "SINGLE",
+    tracks: [
+      {
+        artists: [
+          {
+            name: "Kygo"
+          }
+        ],
+        featuring: [
+          {
+            name: "Sandro Cavazza"
+          }
+        ],
+        name: "Happy Now",
+        duration: 211,
+        trackNumber: 1
+      }
+    ]
+  },
+  {
+    artists: [
+      {
+        name: "Steve Aoki"
+      }
+    ],
+    genres: ["Dance"],
+    name: "Neon Future III",
+    releaseDate: "2018-11-09T00:00:00.000Z",
+    releaseType: "SINGLE",
+    tracks: [
+      {
+        artists: [
+          {
+            name: "Steve Aoki"
+          }
+        ],
+        featuring: [],
+        name: "Neon Future III (Intro)",
+        duration: 256,
+        trackNumber: 1
+      },
+      {
+        artists: [
+          {
+            name: "Steve Aoki"
+          }
+        ],
+        featuring: [
+          {
+            name: "Louis Tomlinson"
+          }
+        ],
+        name: "Just Hold On",
+        duration: 199,
+        trackNumber: 2
+      },
+      {
+        artists: [
+          {
+            name: "Steve Aoki"
+          }
+        ],
+        featuring: [
+          {
+            name: "BTS"
+          }
+        ],
+        name: "Waste It on Me",
+        duration: 193,
+        trackNumber: 3
+      },
+      {
+        artists: [
+          {
+            name: "Steve Aoki"
+          }
+        ],
+        featuring: [
+          {
+            name: "Kiiara"
+          }
+        ],
+        name: "Be Somebody",
+        duration: 198,
+        trackNumber: 4
+      },
+      {
+        artists: [
+          {
+            name: "Steve Aoki"
+          }
+        ],
+        featuring: [
+          {
+            name: "Lil Yatchy"
+          },
+          {
+            name: "AJR"
+          }
+        ],
+        name: "Pretender",
+        duration: 189,
+        trackNumber: 5
+      },
+      {
+        artists: [
+          {
+            name: "Steve Aoki"
+          }
+        ],
+        featuring: [
+          {
+            name: "MIke Posner"
+          }
+        ],
+        name: "A Lover and a Memory",
+        duration: 208,
+        trackNumber: 6
+      },
+      {
+        artists: [
+          {
+            name: "Steve Aoki"
+          }
+        ],
+        featuring: [
+          {
+            name: "blink-182"
+          }
+        ],
+        name: "Why Are We so Broken",
+        duration: 228,
+        trackNumber: 7
+      },
+      {
+        artists: [
+          {
+            name: "Steve Aoki"
+          }
+        ],
+        featuring: [
+          {
+            name: "Jim Adkins"
+          }
+        ],
+        name: "Golden Days",
+        duration: 204,
+        trackNumber: 8
+      },
+      {
+        artists: [
+          {
+            name: "Steve Aoki"
+          }
+        ],
+        featuring: [
+          {
+            name: "Lady Antebellum"
+          }
+        ],
+        name: "Our Love Glows",
+        duration: 173,
+        trackNumber: 9
+      },
+      {
+        artists: [
+          {
+            name: "Steve Aoki"
+          }
+        ],
+        featuring: [
+          {
+            name: "Era Istrefi"
+          }
+        ],
+        name: "Anything More",
+        duration: 180,
+        trackNumber: 10
+      },
+      {
+        artists: [
+          {
+            name: "Steve Aoki"
+          },
+          {
+            name: "Lauren Jauregui"
+          }
+        ],
+        featuring: [],
+        name: "All Night",
+        duration: 205,
+        trackNumber: 11
+      },
+      {
+        artists: [
+          {
+            name: "Steve Aoki"
+          }
+        ],
+        featuring: [
+          {
+            name: "Bella Thorne"
+          }
+        ],
+        name: "Do Not Disturb",
+        duration: 164,
+        trackNumber: 12
+      },
+      {
+        artists: [
+          {
+            name: "Steve Aoki"
+          }
+        ],
+        featuring: [
+          {
+            name: "Ina Wroldsen"
+          }
+        ],
+        name: "Lie to Me",
+        duration: 179,
+        trackNumber: 13
+      },
+      {
+        artists: [
+          {
+            name: "Steve Aoki"
+          },
+          {
+            name: "Daddy Yankee"
+          },
+          {
+            name: "Play-N-Skillz"
+          },
+          {
+            name: "Elvis Crespo"
+          }
+        ],
+        featuring: [],
+        name: "Azukita",
+        duration: 226,
+        trackNumber: 14
+      },
+      {
+        artists: [
+          {
+            name: "Steve Aoki"
+          },
+          {
+            name: "TWIIG"
+          }
+        ],
+        featuring: [],
+        name: "Hoovela",
+        duration: 207,
+        trackNumber: 15
+      },
+      {
+        artists: [
+          {
+            name: "Steve Aoki"
+          },
+          {
+            name: "Don Diablo"
+          },
+          {
+            name: "Lush & Simon"
+          }
+        ],
+        featuring: [
+          {
+            name: "BullySongs"
+          }
+        ],
+        name: "What We Started",
+        duration: 203,
+        trackNumber: 16
+      },
+      {
+        artists: [
+          {
+            name: "Steve Aoki"
+          }
+        ],
+        featuring: [
+          {
+            name: "Bill Nye"
+          }
+        ],
+        name: "Noble Gas",
+        duration: 219,
+        trackNumber: 17
+      }
+    ]
+  }
+];
--- packages/backend/prisma/seed.ts
+++ packages/backend/prisma/seed.ts
@@ -2,6 +2,7 @@ import { prisma } from "../src/generated/prisma-client";
 import { createAlbum } from "../src/schema/Album/Album.service";
 import { createUser } from "../src/schema/User/User.service";
 import { Context } from "../src/typings";
+import albumsToCreate from "./data/albums";
 
 let context: Context = {
   prisma,
@@ -32,296 +33,24 @@ async function main() {
     }
   };
 
-  const albumInput = {
-    artwork: {
-      url: "texuohrqozjarva7limv"
-    },
-    artists: [
-      {
-        name: "Steve Aoki"
-      }
-    ],
-    genres: ["Dance"],
-    name: "Neon Future III",
-    releaseDate: "2018-11-09T00:00:00.000Z",
-    releaseType: "SINGLE",
-    tracks: [
-      {
-        artists: [
-          {
-            name: "Steve Aoki"
-          }
-        ],
-        featuring: [],
-        name: "Neon Future III (Intro)",
-        duration: 256,
-        trackNumber: 1
-      },
-      {
-        artists: [
-          {
-            name: "Steve Aoki"
-          }
-        ],
-        featuring: [
-          {
-            name: "Louis Tomlinson"
-          }
-        ],
-        name: "Just Hold On",
-        duration: 199,
-        trackNumber: 2
-      },
-      {
-        artists: [
-          {
-            name: "Steve Aoki"
-          }
-        ],
-        featuring: [
-          {
-            name: "BTS"
-          }
-        ],
-        name: "Waste It on Me",
-        duration: 193,
-        trackNumber: 3
-      },
-      {
-        artists: [
-          {
-            name: "Steve Aoki"
-          }
-        ],
-        featuring: [
-          {
-            name: "Kiiara"
-          }
-        ],
-        name: "Be Somebody",
-        duration: 198,
-        trackNumber: 4
-      },
-      {
-        artists: [
-          {
-            name: "Steve Aoki"
-          }
-        ],
-        featuring: [
-          {
-            name: "Lil Yatchy"
-          },
-          {
-            name: "AJR"
-          }
-        ],
-        name: "Pretender",
-        duration: 189,
-        trackNumber: 5
-      },
-      {
-        artists: [
-          {
-            name: "Steve Aoki"
-          }
-        ],
-        featuring: [
-          {
-            name: "MIke Posner"
-          }
-        ],
-        name: "A Lover and a Memory",
-        duration: 208,
-        trackNumber: 6
-      },
-      {
-        artists: [
-          {
-            name: "Steve Aoki"
-          }
-        ],
-        featuring: [
-          {
-            name: "blink-182"
-          }
-        ],
-        name: "Why Are We so Broken",
-        duration: 228,
-        trackNumber: 7
-      },
-      {
-        artists: [
-          {
-            name: "Steve Aoki"
-          }
-        ],
-        featuring: [
-          {
-            name: "Jim Adkins"
-          }
-        ],
-        name: "Golden Days",
-        duration: 204,
-        trackNumber: 8
-      },
-      {
-        artists: [
-          {
-            name: "Steve Aoki"
-          }
-        ],
-        featuring: [
-          {
-            name: "Lady Antebellum"
-          }
-        ],
-        name: "Our Love Glows",
-        duration: 173,
-        trackNumber: 9
-      },
-      {
-        artists: [
-          {
-            name: "Steve Aoki"
-          }
-        ],
-        featuring: [
-          {
-            name: "Era Istrefi"
-          }
-        ],
-        name: "Anything More",
-        duration: 180,
-        trackNumber: 10
-      },
-      {
-        artists: [
-          {
-            name: "Steve Aoki"
-          },
-          {
-            name: "Lauren Jauregui"
-          }
-        ],
-        featuring: [],
-        name: "All Night",
-        duration: 205,
-        trackNumber: 11
-      },
-      {
-        artists: [
-          {
-            name: "Steve Aoki"
-          }
-        ],
-        featuring: [
-          {
-            name: "Bella Thorne"
-          }
-        ],
-        name: "Do Not Disturb",
-        duration: 164,
-        trackNumber: 12
-      },
-      {
-        artists: [
-          {
-            name: "Steve Aoki"
-          }
-        ],
-        featuring: [
-          {
-            name: "Ina Wroldsen"
-          }
-        ],
-        name: "Lie to Me",
-        duration: 179,
-        trackNumber: 13
-      },
-      {
-        artists: [
-          {
-            name: "Steve Aoki"
-          },
-          {
-            name: "Daddy Yankee"
-          },
-          {
-            name: "Play-N-Skillz"
-          },
-          {
-            name: "Elvis Crespo"
-          }
-        ],
-        featuring: [],
-        name: "Azukita",
-        duration: 226,
-        trackNumber: 14
-      },
-      {
-        artists: [
-          {
-            name: "Steve Aoki"
-          },
-          {
-            name: "TWIIG"
-          }
-        ],
-        featuring: [],
-        name: "Hoovela",
-        duration: 207,
-        trackNumber: 15
-      },
-      {
-        artists: [
-          {
-            name: "Steve Aoki"
-          },
-          {
-            name: "Don Diablo"
-          },
-          {
-            name: "Lush & Simon"
-          }
-        ],
-        featuring: [
-          {
-            name: "BullySongs"
-          }
-        ],
-        name: "What We Started",
-        duration: 203,
-        trackNumber: 16
-      },
-      {
-        artists: [
-          {
-            name: "Steve Aoki"
-          }
-        ],
-        featuring: [
-          {
-            name: "Bill Nye"
-          }
-        ],
-        name: "Noble Gas",
-        duration: 219,
-        trackNumber: 17
-      }
-    ]
-  };
+  /**
+   * Create albums
+   */
 
-  console.log("Creating album...");
+  console.log("Creating albums...");
 
-  await createAlbum(
-    {
-      ...albumInput
-    },
-    context
-  );
+  albumsToCreate.forEach(async album => {
+    console.log(`  Creating ${album.name}`);
 
-  console.log("Album created...");
+    await createAlbum(
+      {
+        ...album
+      },
+      context
+    );
+  });
+
+  console.log("Albums created...");
 }
 
 main().catch(e => console.error(e.message));
